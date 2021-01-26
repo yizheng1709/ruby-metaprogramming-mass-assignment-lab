b0e49bb9bd8a1e18269af61b4c915a41efd024b3
Eva <yi.zheng1709@gmail.com>
2021-01-26 10:57:53 -0500

    commit tree 237f783a609dcb43496a914a096338cfa3f30543

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,8 @@
 class Person
-  #your code here
+  def initialize(attributes)
+    attributes.each do |key, value|
+      self.class.attr_accessor(key) #attr_accessor is a class method
+      self.send(("#{key}="), value)  #interpolate key cuz it'll be the name of the setter/getter methods
+    end 
+  end 
 end
